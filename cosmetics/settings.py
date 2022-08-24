4259d6fbdbd5bf7043d27436579632d84f782a5c
Gideon Odekina <gideonodekina1@gmail.com>
2022-08-24 22:57:17 +0100

    Set DEBUG based on environment variables.

--- cosmetics/settings.py
+++ cosmetics/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0vxq_li)vjql8n94)3uc9zctsr6y+idiwzl^sia^)=-2spivxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
